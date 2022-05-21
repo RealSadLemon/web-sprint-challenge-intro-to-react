674334276db8969fc096f8cc4a5de89fef70359a
RealSadLemon <esmodearburk@gmail.com>
2022-05-20 20:12:02 -0500

    First commit, added forEach loop and connected API for data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+import Character from './components/Character';
+import axios from 'axios';
 
 const App = () => {
+  const [data, setData] = useState([]);
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -8,9 +12,26 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  useEffect(()=>{axios.get('https://swapi.dev/api/people/')
+    .then(res=>{
+    setData(res.data);
+    })
+    .catch(err=>{
+    return(
+      <div>The API did not connect.</div>
+    )
+  })}, [])
+
+  
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {data.forEach(character=>{
+        return(
+          <Character />
+        )
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,8 @@
 // Write your Character component here
+import React from 'react';
+
+const Character = props =>{
+
+}
+
+export default Character;
